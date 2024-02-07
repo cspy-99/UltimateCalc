ce9a49c943d8241a89c72a48c818d036d784ed94
Kacper Popiński <kacpi5a5@o2.pl>
2024-02-07 01:13:20 +0100

    newProject:P

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('.js-button');
+const result = document.querySelector('.js-result');
+
+button.addEventListener('click', function () {
+    result.innerHTML = 'Hello World!';
+});
+
